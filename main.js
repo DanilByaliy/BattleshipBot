632601c99f53052b51a98bd77b5205cf3fd1ca7a
DanilByaliy <byaliy03@gmail.com>
2022-02-19 21:45:55 +0200

    added the function of drawing ships on the playing field (closes #5)

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-const { drawingGameBoard, drawingGoodShot,
-  drawingBadShot } = require('./lib/drawing.js');
+const { drawingGameBoard, drawingGoodShot } = require('./lib/drawing.js');
+const { placeAllShips } = require('./lib/game.js');
 const token = process.env.MY_TELEGRAM_BOT_TOKEN;
 const chatId = process.env.MY_TELEGRAM_CHAT_ID;
 
@@ -21,9 +21,9 @@ function sendPicture() {
   console.log(chatId);
   bot.sendPhoto(chatId, './board.png');
 }
+
 // Test
 
 drawingGameBoard(sendPicture)
-  .then(() => drawingGoodShot(sendPicture, 1, 1))
-  .then(() => drawingBadShot(sendPicture, 2, 2))
-  .then(() => drawingGoodShot(sendPicture, 3, 2));
+  .then(() => placeAllShips(sendPicture));
+// .then(() => drawingGoodShot(sendPicture, 1, 1));
