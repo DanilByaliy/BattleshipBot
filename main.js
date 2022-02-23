09a668aeb54ccf2c1181d6e0cd6153ed3235cfb1
DanilByaliy <byaliy03@gmail.com>
2022-02-23 22:45:42 +0200

    Add listeners to run the bot and the game
    Added a listener to run the bot, if this happens, the username and chat
    ID will be recorded in the user base object. Added listener to start
    the game, when this happens, there will be a check for another user
    in the database, if he is, he will receive a message asking to start
    the game. If there is no other user in the database, the corresponding
    message will be sent to the first user.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = MY_TELEGRAM_BOT_TOKEN;
 
+const userBase = new Object();
+
 console.log('Bot has been started....');
 
 const bot = new TelegramBot(token, {
@@ -11,4 +13,24 @@ const bot = new TelegramBot(token, {
       timeout: 10
     }
   }
+});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const username = msg.chat.username;
+  userBase[username] = chatId;
+  bot.sendMessage(chatId, username + ' : ' + chatId);
+});
+
+bot.onText(/\/game (.+)/, (msg, [source, match]) => {
+  const chatId = msg.chat.id;
+  const username = msg.chat.username;
+  const user = match.slice(1);
+
+  if (!Object.keys(userBase).includes(user)) {
+    bot.sendMessage(chatId, `Користувач @${user} не використовує цього бота`);
+    return;
+  }
+  bot.sendMessage(chatId, 'Виклик кинуто');
+  bot.sendMessage(userBase[user], `Прийняти виклик від @${username}?`);
 });
