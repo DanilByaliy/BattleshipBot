06278aaef75e259972e3a9962f82f5b8e489e842
DanilByaliy <byaliy03@gmail.com>
2022-12-13 23:34:07 +0200

    Add features for placing ships and field edges

--- server/src/services/gameService.js
+++ server/src/services/gameService.js
@@ -19,10 +19,31 @@ class Field {
     createEmptyFieldSizeOf(size) {
         let field = [];
         for (let i = 0; i < size; i++) {
-          const row = new Array(size);
-          row.fill(0);
-          field.push(row);
+            const row = new Array(size);
+            row.fill(0);
+            field.push(row);
         }
         return field;
-    }   
+    }
+
+    placeTwoDeskShips() {
+        for (let i = 0; i < 2; i++) {
+            this.placeTwoDeskShip(i);
+        }
+    }
+
+    placeOneDeskShips() {
+        for (let i = 0; i < 3; i++) {
+            this.placeOneDeskShip(i);
+        }
+    }
+
+    createEnvelope() {
+        for (let i = 0; i < 9; i++) {
+            this.value[0][i] = 9;
+            this.value[8][i] = 9;
+            this.value[i][0] = 9;
+            this.value[i][8] = 9;
+        }
+    }
 }
