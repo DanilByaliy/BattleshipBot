78b9eaeba1823c9496dcf92cd931971d0fee843b
DanilByaliy <byaliy03@gmail.com>
2022-12-13 23:27:20 +0200

    Add the Fields class and the field creation function

---
+++ server/src/services/gameService.js
@@ -0,0 +1,28 @@
+'use strict'
+
+class Field {
+    value
+
+    createRandomField() {
+        this.setInitialField()
+        this.placeThreeDeskShip();
+        this.placeTwoDeskShips();
+        this.placeOneDeskShips();
+    }
+
+    setInitialField() {
+        const field = this.createEmptyFieldSizeOf(9);
+        this.value = field;
+        this.createEnvelope();
+    }
+
+    createEmptyFieldSizeOf(size) {
+        let field = [];
+        for (let i = 0; i < size; i++) {
+          const row = new Array(size);
+          row.fill(0);
+          field.push(row);
+        }
+        return field;
+    }   
+}
