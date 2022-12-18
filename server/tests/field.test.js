29f62b1b33822d57035375e331069d6f7871f654
DanilByaliy <byaliy03@gmail.com>
2022-12-18 14:31:31 +0200

    Add tests for location functions of three deck ships

--- server/tests/field.test.js
+++ server/tests/field.test.js
@@ -52,4 +52,54 @@ describe('Field class:', () => {
         expect(coordinateC4).toEqual({x: 3, y: 4});
         expect(coordinateG7).toEqual({x: 7, y: 7});
     })
+
+    test(`The placeThreeDeskShipHorizontallyFrom function should place a ship 
+            with three decks horizontally from the specified coordinate`, () => {
+        const field = new Field();
+        field.setInitialField();
+
+        field.placeThreeDeskShipHorisontallyFrom(2, 3);
+        const contentOfCellB3 = field.getCellContent('b3');
+        const contentOfCellB4 = field.getCellContent('b4');
+        const contentOfCellB5 = field.getCellContent('b5');
+        const gameBoard = field.getField();
+
+        expect(contentOfCellB3).toEqual('threeDeck');
+        expect(contentOfCellB4).toEqual('threeDeck');
+        expect(contentOfCellB5).toEqual('threeDeck');
+        expect(gameBoard).toEqual([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]);
+    })
+
+    test(`The placeThreeDeskShipVerticallyFrom function should place a ship 
+            with three decks vertically from the specified coordinate`, () => {
+        const field = new Field();
+
+        field.setInitialField();
+        field.placeThreeDeskShipVerticallyFrom(2, 3);
+        const contentOfCellB3 = field.getCellContent('b3');
+        const contentOfCellC3 = field.getCellContent('c3');
+        const contentOfCellD3 = field.getCellContent('d3');
+        const gameBoard = field.getField();
+
+        expect(contentOfCellB3).toEqual('threeDeck');
+        expect(contentOfCellC3).toEqual('threeDeck');
+        expect(contentOfCellD3).toEqual('threeDeck');
+        expect(gameBoard).toEqual([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]);
+    })
 })
