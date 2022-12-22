7b39e4ec280a2383cda17df75c2af5f20f9bf466
DanilByaliy <byaliy03@gmail.com>
2022-12-22 21:33:07 +0200

    Add a method of drawing the initial field

--- client/src/services/drawingService.js
+++ client/src/services/drawingService.js
@@ -36,4 +36,35 @@ class DrawingService {
             ctx: ctx,
         };
     }
+
+    drawingGameField(n, ctx) {
+        ctx.fillRect(2400 * (n - 1) + 130, 130, 2140, 2140);
+        ctx.clearRect(2400 * (n - 1) + 150, 150, 2100, 2100);
+        drawingLetters(n - 1, ctx);
+        drawingNumbers(n - 1, ctx);
+      
+        for (let i = 1; i < 7; i++) {
+            drawingVerticalLines(ctx, i, n - 1);
+            drawingHorisontalLines(ctx, i, n - 1);
+        }
+    }
+
+    drawingVerticalLines(ctx, i, n) {
+        ctx.beginPath();
+        ctx.moveTo(2400 * n + 150 + 300 * i, 2250);
+        ctx.lineTo(2400 * n + 150 + 300 * i, 150);
+        ctx.lineTo(2400 * n + 165 + 300 * i, 2250);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    drawingHorisontalLines(ctx, i, n) {
+        ctx.beginPath();
+        ctx.moveTo(2400 * n + 2250, 150 + 300 * i);
+        ctx.lineTo(2400 * n + 150, 150 + 300 * i);
+        ctx.lineTo(2400 * n + 2250, 165 + 300 * i);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
 }
